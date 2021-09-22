69d56d38a3d9786ebf40f88b2976ca81e219d7dd
Eduardo Almeida <contato.eduardopex@hotmail.com>
2021-09-22 08:44:37 -0300

    feat(LIST_POKEMONS_MOCKED): created a list of availables pokemons into app.

--- app/environments/environment.js
+++ app/environments/environment.js
@@ -8,8 +8,46 @@ export const environment = {
         selectPokemon: 'select-pokemon.html',
     },
 
+    assets: {
+        image: '../app/assets/img/',
+        pokemon: '../app/assets/img/pokemon/',
+    },
+
     socket: {
         baseUrl: "https://front-br-challenges.web.app/api/v2/",
     }
 }
+
+export const LIST_POKEMONS_MOCKED = {
+    PIKACHU: {
+        id: 1,
+        name: 'Pikachu',
+        color: '#f4c430',
+        alternativeColor: '#fffdd0',
+        imageUrl: environment.assets.pokemon + 'pikachu.png',
+    },
+    CHARMANDER: {
+        id: 2,
+        name: 'Charmander',
+        color: '#b22222',
+        alternativeColor: '#de3163',
+        imageUrl: environment.assets.pokemon + 'charmander.png',
+    },
+    BULLBASAUR: {
+        id: 3,
+        name: 'Bullbasaur',
+        color: '#8b008b',
+        alternativeColor: '#9acd32',
+        imageUrl: environment.assets.pokemon + 'bullbasaur.png',
+    },
+    WARTORTLE: {
+        id: 4,
+        name: 'Wartortle',
+        color: '#87cefa',
+        alternativeColor: '#3db0f7',
+        imageUrl: environment.assets.pokemon + 'wartortle.png',
+    },
+};
+
 Object.freeze(environment);
+Object.freeze(LIST_POKEMONS_MOCKED);
