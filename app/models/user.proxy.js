76616ffe46798621934dd6348449ace119a90b7d
Eduardo Almeida <contato.eduardopex@hotmail.com>
2021-10-05 16:38:58 -0300

    feat(User): started to create a way to get user ip.

--- app/models/user.proxy.js
+++ app/models/user.proxy.js
@@ -4,7 +4,25 @@ export class User {
     this.name = "";
     this.pokemon = null;
     this.isPlaying = false;
+    // this.ip = await this._getUserIp();
     // this.pokemonName = null; //Pokemon is tracked by it name
   }
 
+  get id() {
+    if (!this.name)
+      return '';
+
+    if (!this.ip)
+      this.ip = await this._getUserIp();
+
+    return `${this.name.toLocaleLowerCase()}#${this.ip}`;
+  }
+
+  async _getUserIp() {
+    const data = await fetch('https://www.cloudflare.com/cdn-cgi/trace');
+    return data.trim().split('\n').reduce(function (obj, pair) {
+      pair = pair.split('=');
+      return obj[pair[0]] = pair[1], obj;
+    }, {});
+  }
 }
