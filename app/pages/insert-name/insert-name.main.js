427f476e7a2f9fcc61a749d8ff3b0156c7970062
Eduardo Almeida <eduardo.oliveira@facens.br>
2021-10-17 17:16:49 -0300

    refactor(insert-name): updated page style and rules

---
+++ app/pages/insert-name/insert-name.main.js
@@ -0,0 +1,18 @@
+// #region Imports
+
+import 'regenerator-runtime/runtime';
+
+import * as ButtonsEvents from './buttons.events';
+import { PageBuilderComponent } from '../../components/page-builder.component';
+
+// #endregion Imports
+
+// #region OnInit
+
+const pageBuilder = new PageBuilderComponent({
+  listLinks: [],
+  listScripts: [],
+});
+
+// #endregion OnInit
+
